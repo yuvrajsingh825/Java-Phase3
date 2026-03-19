86fbb7cece636f50545e83cd0fed83a9571ec251
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-03-19 19:52:11 +0530

    update

---
+++ Throws.java
@@ -0,0 +1,8 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Throws {
+    public static void main(String[] args) throws IOException {
+          FileReader file = new FileReader("abc.txt");
+    }
+}
