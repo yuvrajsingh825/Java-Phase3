f72c0618a43959d9bd12d2852a2bb7f41cb1b94d
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-03-19 18:48:33 +0530

    update

--- exception.java
+++ exception.java
@@ -1,7 +1,14 @@
 public class exception {
-    public static void main(String[] args) {
-        int a = 10;
-        int y = 0;
-        
+    public static void main(String[]args){
+    try {
+    int a = 10;
+    int b = 0;
+
+    System.out.println(a / b);
+} catch (ArithmeticException e) {
+    System.out.println("Error handled");
+}
+
+System.out.println("Program continues...");
     }
 }
