1734dc3550c42f73fa3ffbc377d4ef4e9fcd8cb2
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-03-28 16:40:02 +0530

    add

---
+++ Generics.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Generics {
+    public static void main(String[]args){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("rahul");
+        list.add("Kamlesh");
+        list.add("ram");
+        list.add("monu");
+        System.out.println(list);
+    }
+}
