32b53a01ddb12f9f406cfe4f40cb1a652f041f4c
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-04-01 19:49:20 +0530

    update

--- StreamSquare.java
+++ StreamSquare.java
@@ -4,6 +4,7 @@ public class StreamSquare {
     List<Integer> list = Arrays.asList(1,2,3,4,5,6);   
     
     list.stream()
+         .filter(x -> x % 2 == 0)
        .map(x->x*x)
         .forEach(System.out::println);
     }
